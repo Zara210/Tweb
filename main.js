e5756b771d359d3b36ed881f04c14083a6ccd52f
Zara <135708561+Zara210@users.noreply.github.com>
2024-12-21 19:45:27 +0000

    Correção de bug logo tap no scroll e acabar detalhes no alerta banner

--- main.js
+++ main.js
@@ -1,17 +1,48 @@
-window.onscroll = function () {atualizarNavbar();};
+window.onscroll = function () {
+    atualizarNavbar();
+};
+
+window.onresize = function () {
+    atualizarNavbar(); // Atualiza a navbar também no redimensionamento
+};
 
 function atualizarNavbar() {
     const navbar = document.querySelector('nav');
     const profileIcon = document.querySelector('.nav-buttons_pc .login-btn img');
-    const navText = document.querySelector('.nav-buttons_pc .nav-text'); 
+    const logoBranco = document.querySelector('.logo_branco'); // Seleciona o logo branco
+    const logoNormal = document.querySelector('.logo'); // Seleciona o logo normal
 
-    if (window.scrollY > 20) {
-        navbar.classList.add('navbar-scroll'); // Adiciona a classe do scroll
+    // Quando o usuário rolar mais de 20px
+    if (window.scrollY > 15) {
+        navbar.classList.add('navbar-scroll'); // Adiciona a classe "navbar-scroll"
         profileIcon.src = 'img/profile_verde.jpg'; // Altera o ícone para verde
-        navText.style.color = 'black'; // Altera a cor do texto para branco
+
+        // Troca o logo apenas se a tela for maior que 1124px
+        if (window.innerWidth > 1124) {
+            logoBranco.style.display = 'none'; // Esconde o logo branco
+            logoNormal.style.display = 'block'; // Exibe o logo normal
+        }
+
     } else {
-        navbar.classList.remove('navbar-scroll'); // Remove a classe do scroll
+        navbar.classList.remove('navbar-scroll'); // Remove a classe "navbar-scroll"
         profileIcon.src = 'img/profile_branco.png'; // Volta para o ícone branco
-        navText.style.color = ''; // Remove a cor personalizada 
+
+        // Troca o logo apenas se a tela for maior que 1124px
+        if (window.innerWidth > 1124) {
+            logoBranco.style.display = 'block'; // Exibe o logo branco
+            logoNormal.style.display = 'none'; // Esconde o logo normal
+        }
+    }
+
+    // Ao redimensionar a tela, esconde o logo branco se for menor que 1124px
+    if (window.innerWidth <= 1124) {
+        logoBranco.style.display = 'none'; // Esconde o logo branco
+        logoNormal.style.display = 'none'; // Esconde o logo normal
+    }
+
+    // Adiciona a condição para telas menores que 768px
+    if (window.innerWidth < 1024) {
+        logoBranco.style.display = 'none'; // Esconde o logo branco
+        logoNormal.style.display = 'block'; // Esconde o logo normal
     }
 }
