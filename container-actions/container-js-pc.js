94d5d6b7024644e1e70cb9bb8f52348dbc5b3cb7
Zara <135708561+Zara210@users.noreply.github.com>
2024-12-24 13:51:58 +0000

    pequena correç

--- container-actions/container-js-pc.js
+++ container-actions/container-js-pc.js
@@ -2,7 +2,10 @@
 const reservarVooHeader = document.getElementById("reservar-voo-header");
 const reservaVooContainer = document.getElementById("reserva-voo-container");
 
-// Adicionar evento de clique no cabeçalho
+const minhasViagensHeader = document.getElementById("minhas-viagens-header");
+const minhasViagensContainer = document.getElementById("minhas-viagens-container");
+
+// Adicionar evento de clique no cabeçalho "Reservar um voo"
 reservarVooHeader.addEventListener("click", () => {
     if (reservaVooContainer.classList.contains("expandido")) {
         // Remover a classe para colapsar
@@ -12,5 +15,18 @@ reservarVooHeader.addEventListener("click", () => {
         // Adicionar a classe para expandir
         reservaVooContainer.classList.add("expandido");
         reservaVooContainer.style.display = "flex"; // Exibir como flex
+    }
+});
+
+// Adicionar evento de clique no cabeçalho "As minhas viagens"
+minhasViagensHeader.addEventListener("click", () => {
+    if (minhasViagensContainer.classList.contains("expandido")) {
+        // Remover a classe para colapsar
+        minhasViagensContainer.classList.remove("expandido");
+        minhasViagensContainer.style.display = "none"; // Ocultar
+    } else {
+        // Adicionar a classe para expandir
+        minhasViagensContainer.classList.add("expandido");
+        minhasViagensContainer.style.display = "flex"; // Exibir como flex
     }
 });
